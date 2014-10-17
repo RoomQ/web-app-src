24465bb498d3a14e510fa027a56557df97e9da0e
Kyriakos Spyropoulos <spirkir@yahoo.gr>
2014-10-17 16:52:33 +0100

    Learning laravel

---
+++ app/routes.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('test', function()
+{
+	$name='Kyriakos';
+    return View::make('test')->with('my_name',$name);
+});
+
+Route::get('users2', function()
+{
+    return View::make('users');
+});
+
+Route::get('usersCtrl', 'PagesController@home');
+Route::get('about', 'PagesController@about');
+Route::get('db', function(){
+	$users = DB::table('users')->where('username','!=','JeffreyWay')->get();
+	$user = DB::table('users')->find(1);
+
+	return $users;
+});
